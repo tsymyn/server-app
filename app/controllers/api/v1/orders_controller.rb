23935385ddac2029bfa7e085c6e3faa3cf6a76a6
tsymyn <noah.erickson@gmail.com>
2016-03-07 04:24:24 +0000

    Working GET route and error handling for order # not in db

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+  
+  def show
+    if Order.exists?(params[:id])
+        @order = Order.find(params[:id])
+    else
+      @order = Order.new(id: params[:id], status: "Invalid order number")
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  #get 'orders/index'
-  #resources :orders, only: [:index]
-  # API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :orders
+      resources :orders, only: [:index, :show]
     end
   end
 end
